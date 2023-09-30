f1898af2d72becb2894a45014eb06fca82a71899
suheoon <ssheon0812@naver.com>
2023-09-30 20:22:31 +0900

    feat: solve again maze_runner

--- suheoon/code_tree_maze_runner.cpp
+++ suheoon/code_tree_maze_runner.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 빈칸, 벽, 출구
-// 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형
-// r 작은것, c 작은 것
-// 모든 참가자들의 이동 거리 합과 출구 좌표 출력
-// 미로, 내구도, 사람들
-
 struct Person {
     int x;
     int y;
@@ -14,22 +8,28 @@ struct Person {
 };
 
 int n, m, k;
-int maze[11][11]; // 0빈칸, 1이상 내구도
-vector<Person> v;
+
+int maze[11][11]; // 미로의 내구도
+int temp[11][11];
+vector<Person> v; // 사람의 정보
 pair<int, int> e; // 출구의 좌표
+int sx, sy, square_size; // 정사각형 정보
 int cnt; // 탈출한 사람 수
 int dist; // 총 움직인 거리
 
+// 상, 하, 좌, 우
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 void move() {
     for (int i = 0; i < v.size(); i++) {
-        if (v[i].finish) continue;
+        if (v[i].finish) {
+            continue;
+        }
         int cx = v[i].x;
         int cy = v[i].y;
         int dist1 = abs(cx - e.first) + abs(cy - e.second);
-        // 움직일 방향 찾기 (상, 하, 좌, 우)
+        // 움직일 방향 찾기
         for (int dir = 0; dir < 4; dir++) {
             int nx = cx + dx[dir];
             int ny = cy + dy[dir];
@@ -49,80 +49,106 @@ void move() {
     }
 }
 
-vector<pair<int, int>> find_rectangle() {
-    Person mn_p;
-    int mn = 987654321;
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i].finish) continue;
-        int tmp = abs(e.first - v[i].x) + abs(e.second - v[i].y);
-        if (tmp < mn) {
-            mn_p = v[i];
-            mn = tmp;
-        }
-    }
-
-    int length = max(abs(mn_p.x - e.first), abs(mn_p.y - e.second));
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i <= e.first && e.first <= i + length && j <= e.second && e.second <= j + length) {
-                int cnt = 0;
-                for (int k = 0; k < v.size(); k++) {
-                    if (v[k].finish) continue;
-                    if (i <= v[k].x && v[k].x <= i + length && j <= v[k].y && v[k].y <= j + length) cnt++; 
+void find_square() {
+    // 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형
+    for (int sz = 2; sz <= n; sz++) {
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                // 출구가 포함 돼 있는지 확인
+                if (!(x <= e.first && e.first < x + sz && y <= e.second && e.second < y + sz)) {
+                    continue;
                 }
-                if (cnt) return {{i, j}, {i + length, j + length}};
+                
+                bool flag = false;
+
+                for (int i = 0; i < v.size(); i++) {
+                    if (v[i].finish) {
+                        continue;
+                    }
+                    if (x <= v[i].x && v[i].x < x + sz && y <= v[i].y && v[i].y < y + sz) {
+                        flag = true;
+                    }
+                }
+
+                if (flag) {
+                    sx = x;
+                    sy = y;
+                    square_size = sz;
+                    return;
+                }
             }
         }
     }
+
+
 }
 
 void rotate() {
-    vector<pair<int, int>> t = find_rectangle();
-
-    int sx = t[0].first;
-    int sy = t[0].second;
-    int ex = t[1].first;
-    int ey = t[1].second;
-
-    // 회전, 내구도 1 깎기
-    int l = ex - sx + 1;
-
-    int temp[11][11] = {0, };
-    for (int i = sx; i <= ex; i++) {
-        for (int j = sy; j <= ey; j++) {
-            temp[j - sy + sx][l - i + sx - 1 + sy] = maze[i][j];
+    // 미로회전, 내구도 1 감소
+    for (int i = sx; i < sx + square_size; i++) {
+        for (int j = sy; j < sy + square_size; j++) {
+            if (maze[i][j]) {
+                maze[i][j]--;
+            }
         }
     }
 
-    for (int i = sx; i <= ex; i++) {
-        for (int j = sy; j <= ey; j++) {
-            if (temp[i][j] > 0) {
-                temp[i][j] -= 1;
-            }
+    for (int i = sx; i < sx + square_size; i++) {
+        for (int j = sy; j < sy + square_size; j++) {
+            int ox = i - sx;
+            int oy = j - sy;
+            
+            int rx = oy;
+            int ry = square_size - ox - 1;
+
+            temp[rx + sx][ry + sy] = maze[i][j];
+        }
+    }
+
+    for (int i = sx; i < sx + square_size; i++) {
+        for (int j = sy; j < sy + square_size; j++) {
             maze[i][j] = temp[i][j];
         }
     }
 
     // 사람 회전
     for (int i = 0; i < v.size(); i++) {
+        if (v[i].finish) {
+            continue;
+        }
+
         int cx = v[i].x;
         int cy = v[i].y;
-        if (sx <= cx && cx <= ex && sy <= cy && cy <= ey) {
-            v[i].x = cy - sy + sx;
-            v[i].y = l - cx + sx -1 + sy;
+        
+        if (!(sx <= cx && cx < sx + square_size && sy <= cy && cy < sy + square_size)) {
+            continue;
         }
+
+        int ox = cx - sx;
+        int oy = cy - sy;
+
+        int rx = oy;
+        int ry = square_size - ox - 1;
+
+        v[i].x = rx + sx;
+        v[i].y = ry + sy;
     }
 
-    // 출구 회전
-    int i = e.first;
-    int j = e.second;
-    e.first = j - sy + sx;
-    e.second = l - i + sx - 1 + sy;
+    // 출구 회전 (항상 회전)
+    int ox = e.first - sx;
+    int oy = e.second - sy;
+
+    int rx = oy;
+    int ry = square_size - ox - 1;
+
+    e.first = rx + sx;
+    e.second = ry + sy;
 }
 
 bool is_finish() {
-    if (cnt == m) return true;
+    if (cnt == m) {
+        return true;
+    }
     return false;
 }
 
@@ -130,30 +156,34 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // 입력 부분
     cin >> n >> m >> k;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> maze[i][j];
         }
     }
+
     for (int i = 0; i < m; i++) {
         int x, y;
         cin >> x >> y;
         v.push_back({x - 1, y - 1, false});
     }
-    int x, y;
-    cin >> x >> y;
-    e.first = x - 1;
-    e.second = y - 1;
-    
-    while(k--) {
+
+    cin >> e.first >> e.second;
+    e.first -= 1;
+    e.second -= 1;
+
+    for (int i = 0; i < k; i++) {        
         move();
-        if (is_finish()) break;
+        if (is_finish()) {
+            break;
+        }
+        find_square();
         rotate();
     }
     cout << dist << '\n';
-    cout << e.first + 1 << " " << e.second + 1 << '\n';
+    cout << e.first + 1 << ' ' << e.second + 1 << '\n';
 
     return 0;
 }
